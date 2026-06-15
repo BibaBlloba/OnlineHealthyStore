feddd7effc3c729c258b9449dd2da1424381b5d4
Долгополов Владислав <vladvaledol@gmail.com>
2026-06-15 15:39:57 +0300

    alembic fix
    add order_items table

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -2,6 +2,7 @@ from .cart import Cart
 from .cartItem import CartItem
 from .category import Category
 from .order import Order
+from .orderItem import OrderItem
 from .payment import Payment
 from .product import Product
 from .productImage import ProductImage
