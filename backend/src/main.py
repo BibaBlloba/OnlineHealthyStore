0b568fb96188f26ab678921a62ab423f21617a66
Долгополов Владислав <vladvaledol@gmail.com>
2026-06-15 11:29:41 +0300

    init server

---
+++ backend/src/main.py
@@ -0,0 +1,12 @@
+import sys
+from pathlib import Path
+
+import uvicorn
+from fastapi import FastAPI
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+app = FastAPI()
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True)
