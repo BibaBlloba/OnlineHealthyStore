75cc1491b9ce9061de6df7f6a2106cf9eb5859a3
Долгополов Владислав <vladvaledol@gmail.com>
2026-06-16 13:52:23 +0300

    product images

--- backend/src/api/products.py
+++ backend/src/api/products.py
@@ -1,4 +1,11 @@
-from fastapi import APIRouter, Body, HTTPException, Response
+from fastapi import APIRouter, HTTPException, UploadFile, File
+import shutil
+from pathlib import Path
+
+from sqlalchemy import delete
+
+from src.models.productImage import ProductImage
+from src.schemas.productImage import ProductImageCreate
 from src.models.product import Product
 from src.api.dependencies import (
     DbDep,
@@ -99,3 +106,40 @@ async def get_product(
         )
 
     return product
+
+
+@router.patch('/{product_id}/image')
+async def update_product_image(
+    product_id: int,
+    db: DbDep,
+    file: UploadFile = File(...),
+    _: dict = AdminOnly,
+):
+    product = await db.products.get_one_or_none(id=product_id)
+
+    if not product:
+        raise HTTPException(
+            status_code=404,
+            detail='Товар не найден',
+        )
+
+    file_path = Path(f'static/products/{file.filename}')
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+
+    with file_path.open('wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    await db.session.execute(
+        delete(ProductImage).where(ProductImage.product_id == product_id)
+    )
+
+    await db.product_images.add(
+        ProductImageCreate(
+            image_url=f'/static/products/{file.filename}',
+            product_id=product_id,
+        )
+    )
+
+    await db.commit()
+
+    return {'status': 'image updated'}
