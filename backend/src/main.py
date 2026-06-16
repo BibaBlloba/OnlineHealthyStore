4a6233c81a025b9ece65ae7efe9508c6ec81b656
Долгополов Владислав <vladvaledol@gmail.com>
2026-06-16 12:53:26 +0300

    auto admin add

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 import sys
 from pathlib import Path
 
@@ -8,10 +9,22 @@ sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.auth import router as router_auth
 from src.api.products import router as router_products
+from src.utils.admin_create import create_admin
+from src.utils.db_manager import DbManager
+from src.database import async_session_maker
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with DbManager(session_factory=async_session_maker) as db:
+        await create_admin(db)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(router_auth)
 app.include_router(router_products)
+
 
 if __name__ == '__main__':
     uvicorn.run(
