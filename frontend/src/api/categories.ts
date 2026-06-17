1844935b2f941d916b59cb4032596cd3731f9af4
Долгополов Владислав <vladvaledol@gmail.com>
2026-06-17 17:08:10 +0300

    category admin modal

--- frontend/src/api/categories.ts
+++ frontend/src/api/categories.ts
@@ -1,6 +1,26 @@
-import { publicApi } from "./client"
+import { privateApi } from "./client"
+
+export interface Category {
+  id: number
+  name: string
+}
 
 export const getCategories = async () => {
-  const res = await publicApi.get("/categories/")
+  const res = await privateApi.get("/categories/")
+  return res.data
+}
+
+export const createCategory = async (name: string) => {
+  const res = await privateApi.post("/categories/", { name })
+  return res.data
+}
+
+export const updateCategory = async (categoryId: number, name: string) => {
+  const res = await privateApi.patch(`/categories/${categoryId}`, { name })
+  return res.data
+}
+
+export const deleteCategory = async (categoryId: number) => {
+  const res = await privateApi.delete(`/categories/${categoryId}`)
   return res.data
 }
